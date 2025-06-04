e814fcdbcc47d9473404ec60179124b44b58c58d
MD. ELIUS <mdelius.nfs@gmail.com>
2025-06-04 13:59:47 +0600

    Delete .github/workflows/stream.sh

--- .github/workflows/stream.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ffmpeg -re -i "https://drive.google.com/uc?export=download&id=1zCmMJVxvtA-eFFUq7s7FfADppSMAhQXw" \
-  -vn -c:a libmp3lame -b:a 128k -f mp3 \
-  icecast://source:rfdjjQJh@link.zeno.fm:80/ghcsb5a7ma0uv
